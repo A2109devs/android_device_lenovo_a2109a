07c1f7564353f0e461bc45b72f0c26178c9245f6
gmarkall <grm08@doc.ic.ac.uk>
2012-12-08 16:16:55 +0000

    ro.hardware is kai, so name init file accordingly.

--- device-common.mk
+++ device-common.mk
@@ -34,7 +34,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 include frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk
 
 PRODUCT_COPY_FILES += \
-    device/lenovo/a2109a/init.a2109a.rc:root/init.a2109a.rc \
+    device/lenovo/a2109a/init.kai.rc:root/init.kai.rc \
     device/lenovo/a2109a/init.tf.rc:root/init.tf.rc \
     device/lenovo/a2109a/init.IdeaTabA2109A_board.usb.rc:root/init.IdeaTabA2109A_board.usb.rc
 
